41f06b16d00fc3fa5952fe3b414bafb905f9afb8
JonBaldwinAlmarez123 <almarezjbm@gmail.com>
2026-09-07 17:33:14 +0800

    add basic routing

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerTitle: "NutriTrack", headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      {" "}
+      <Stack.Screen name="index" />
+      <Stack.Screen name="meals" />
+    </Stack>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,16 @@
 import { globalStyles } from "@/app/styles/global";
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { ScrollView, Text } from "react-native";
+import HomeHeader from "../app/components/HomeHeader";
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.title}>NutriTrack</Text>
-      <Text style={styles.date}>September 7, 2026</Text>
-    </View>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <Link href="/app/meals" style={{ fontSize: 18, color: "#007bff" }}>
+        Go to Meals
+      </Link>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: "#a0a0b0",
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
