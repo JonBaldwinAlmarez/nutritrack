0e19579a277045e793ed68c37da86bb5eeb9f86f
JonBaldwinAlmarez123 <almarezjbm@gmail.com>
2026-09-09 11:33:47 +0800

    Feature: Update Recent Meals To Use Storage Handler

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { ScrollView, Text } from "react-native";
 import HomeHeader from "../components/HomeHeader";
-import NutriGrid from "../components/NutriGrid";
+import MacroGrid from "../components/NutriGrid";
 import RecentMeals from "../components/RecentMeals";
+import { getMeals, Meal } from "../storage/meals";
 import { globalStyles } from "../styles/global";
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log("Loaded meals:", data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>MacroZone</Text>
+      <Text style={globalStyles.title}>Nutritrack</Text>
       <HomeHeader />
-      <NutriGrid />
-      <RecentMeals />
+      <MacroGrid />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
